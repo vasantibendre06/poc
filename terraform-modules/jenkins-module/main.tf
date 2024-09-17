66ee30957dac3b4bc520152a75c8f570248ea66e
Vasanti Bendre <121009809+vasantibendre06@users.noreply.github.com>
2024-09-17 19:37:21 +0530

    Update main.tf

--- terraform-modules/jenkins-module/main.tf
+++ terraform-modules/jenkins-module/main.tf
@@ -11,28 +11,34 @@ terraform {
   }
 }
 
-  resource "null_resource" "install_jenkins" {
-    provisioner "local-exec" {
-      command = <<EOT
-	echo "Updating and upgrading system packages" >> /var/log/user_data.log 2>&1
-	sudo apt update -y >> /var/log/user_data.log 2>&1
-	sudo apt upgrade -y >> /var/log/user_data.log 2>&1
-	# Install OpenJDK
-	echo "Installing OpenJDK 21" >> /var/log/user_data.log 2>&1
-	sudo apt install openjdk-21-jdk -y >> /var/log/user_data.log 2>&1
-	# Add Jenkins repository and install Jenkins
-	echo "Adding Jenkins repository and installing Jenkins" >> /var/log/user_data.log 2>&1
-	sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  	https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key >> /var/log/user_data.log 2>&1
-	echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list >> /var/log/user_data.log 2>&1
-	sudo apt-get update -y >> /var/log/user_data.log 2>&1
-	sudo apt-get install jenkins -y >> /var/log/user_data.log 2>&1
-	sudo systemctl start jenkins >> /var/log/user_data.log 2>&1
-	sudo systemctl enable jenkins >> /var/log/user_data.log 2>&1
-	# Configure Jenkins user permissions
-	echo "Configuring Jenkins user permissions" >> /var/log/user_data.log 2>&1
-	echo 'jenkins ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/jenkins >> /var/log/user_data.log 2>&1
-     EOT
-   }
- }
+resource "null_resource" "install_jenkins" {
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "Updating and upgrading system packages" | sudo tee -a /var/log/user_data.log
+      sudo apt update -y | sudo tee -a /var/log/user_data.log
+      sudo apt upgrade -y | sudo tee -a /var/log/user_data.log
+      
+      # Install OpenJDK
+      echo "Installing OpenJDK 21" | sudo tee -a /var/log/user_data.log
+      sudo apt install openjdk-21-jdk -y | sudo tee -a /var/log/user_data.log
+      
+      # Add Jenkins repository and install Jenkins
+      echo "Adding Jenkins repository and installing Jenkins" | sudo tee -a /var/log/user_data.log
+      sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee -a /var/log/user_data.log
+      echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list | sudo tee -a /var/log/user_data.log
+      sudo apt-get update -y | sudo tee -a /var/log/user_data.log
+      sudo apt-get install jenkins -y | sudo tee -a /var/log/user_data.log
+      
+      # Start Jenkins and enable on boot
+      echo "Starting and enabling Jenkins" | sudo tee -a /var/log/user_data.log
+      sudo systemctl start jenkins | sudo tee -a /var/log/user_data.log
+      sudo systemctl enable jenkins | sudo tee -a /var/log/user_data.log
+      
+      # Configure Jenkins user permissions
+      echo "Configuring Jenkins user permissions" | sudo tee -a /var/log/user_data.log
+      echo 'jenkins ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/jenkins | sudo tee -a /var/log/user_data.log
+    EOT
+  }
+}
+
 
